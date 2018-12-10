c8b702d05138e253996691b02a13f1ea04abd5eb
esac <esac@avogadro>
2018-12-10 21:00:13 +0100

    wrote fancier code, found a bug

--- poem_generator.py
+++ poem_generator.py
@@ -1,80 +1,101 @@
-import numpy as np
+import random
 import string
-import glob
-
-# initial_word = {}  # prob of first word
-# second_word = {}  # prob second word
-# transitions = {}  # prob transitions
 
 
-
-class poems_Generator():
-
+class PoemsGenerator(object):
     def __init__(self):
         self.initial_word = dict()
         self.second_word = dict()
-        self.transitions = dict()
-        self.tokenize()
+        self.transition_probabilities = dict()
+        # self.tokenize_corpora()
+        self.fancy_tokenize_corpora()
         self.normalize_distributions()
 
-    def remove_punctuation(self, my_string):
+    @staticmethod
+    def remove_punctuation(my_string):
         return my_string.translate(str.maketrans('', '', string.punctuation))
 
-    # create array for all possible combinations
-    # key = (the, holidays), value = [with, are, thoughts, with]
-    def possibilities(self, dict, key, value):
-        if key not in dict:
-            dict[key] = []
-        dict[key].append(value)
-
-    def possibilities_prob(self, ls):
-
+    @staticmethod
+    def possibilities(transitions, key, value):
         """
-        :param ls: turn the possible combinatons into probabilities
+            Creates an array for all possible combinations.
+            key = (the, holidays), value = [with, are, thoughts, with]
+        """
+        if key not in transitions:
+            transitions[key] = []
+        transitions[key].append(value)
+
+    @staticmethod
+    def list_to_prob_distribution(ls):
+        """
+        :param ls: turn the possible combinations into probabilities
         :return: value = [with, are, thoughts, with] --> {'with': 0.5, 'are': 0.25, 'thoughts': 0.25}
         """
-        dict = {}
+        transitions = {}
         # l = [dict[word] for word in ls (dict.get(word, 0) +1)]
         for word in ls:
-            dict[word] = dict.get(word, 0) + 1
-        for key, value in dict.items():
-            dict[key] = value / len(ls)
-        return dict
+            transitions[word] = transitions.get(word, 0) + 1
+        for key, value in transitions.items():
+            transitions[key] = value / len(ls)
+        return transitions
 
+    def fancy_tokenize_corpora(self):
+        import collections
+        history = collections.deque(maxlen=2)
+        skipped = []
 
+        for line_nr, line in enumerate(open("robert_frost.txt")):
+            line_formatted = line.rstrip().lower()
+            line_nopunct = self.remove_punctuation(line_formatted)
 
-    def tokenize(self):
-        """
-        :return:
-        """
+            tokens = line_nopunct.split()
+            if len(tokens) == 0:
+                skipped.append(line_nr)
+                continue
 
+            tokens.append("END")
+
+            first_word = tokens.pop(0)
+            self.initial_word[first_word] = self.initial_word.get(first_word, 0) + 1
+            history.append(first_word)
+
+            second_word = tokens.pop(0)
+            self.possibilities(self.second_word, first_word, second_word)
+            history.append(second_word)
+
+            for word in tokens:
+                self.possibilities(self.transition_probabilities, tuple(history), word)
+                history.append(word)
+
+        print('Skipped loading the following lines', skipped)
+
+    def tokenize_corpora(self):
         for line in open("robert_frost.txt"):
-            tokens = self.remove_punctuation(line.rstrip().lower()).split()
+            line = line.rstrip().lower()
+            line = self.remove_punctuation(line)
+            tokens = line.split()
 
             # TODO: revisit this section
-            len_token = len(tokens)
-            for i in range(len_token):
-                word = tokens[i]
-                if i == 0:  # first word
-                    self.initial_word[word] = self.initial_word.get(word, 0) + 1
-                else:
-                    t_1 = tokens[i - 1]  # last word and stop
-                    if i == len_token - 1:
-                        self.possibilities(self.transitions, (t_1, word), "END")
-                    if i == 1:  # second word
-                        self.possibilities(self.second_word, t_1, word)
-                    else:
-                        t_2 = tokens[i - 2]  # others word
-                        self.possibilities(self.transitions, (t_2, t_1), word)
+            first_word = tokens.pop(0)
+            second_word = tokens.pop(0)
 
+            self.initial_word[first_word] = self.initial_word.get(first_word, 0) + 1
+            self.possibilities(self.second_word, first_word, second_word)
 
+            previous_previous_word = first_word
+            previous_word = second_word
 
+            for word in tokens[:-1]:
+                self.possibilities(self.transition_probabilities, (previous_previous_word, previous_word), word)
+
+                previous_previous_word = previous_word
+                previous_word = word
+
+            last_word = tokens[-1]
+            self.possibilities(self.transition_probabilities, (previous_word, last_word), "END")
 
     def normalize_distributions(self):
-
-        """
-        :return: normalize distribution, turn data into prob
-        """
+        """Normalizes distribution, turns data into prob"""
 
         initial_total = sum(self.initial_word.values())  # total starting words
 
@@ -82,35 +103,22 @@ class poems_Generator():
         for key, value in self.initial_word.items():
             self.initial_word[key] = value / initial_total
 
-        # normilize distribution of second words
+        # normalize distribution of second words
         for key, value in self.second_word.items():
-            self.second_word[key] = self.possibilities_prob(value)
+            self.second_word[key] = self.list_to_prob_distribution(value)
 
         # normalize distribution of transitions words
-        for key, value in self.transitions.items():
-            self.transitions[key] = self.possibilities_prob(value)
+        for key, value in self.transition_probabilities.items():
+            self.transition_probabilities[key] = self.list_to_prob_distribution(value)
 
         return initial_total
 
-
-    def sample_word(self, dict):
-
-        """
-        :param dict:
-        :return:
-        """
-        prob = np.random.random()
-        cumulative = 0
-        for key, value in dict.items():
-            cumulative += value
-            if prob < cumulative:
-                return key
-        assert False
+    def sample_word(self, transition_probabilities):
+        words, probs = zip(*list(transition_probabilities.items()))
+        word, = random.choices(words, probs, k=1)
+        return word
 
     def generator(self):
-        """
-        :return:
-        """
         machine_poem = []
 
         word0 = self.sample_word(self.initial_word)
@@ -120,7 +128,7 @@ class poems_Generator():
         machine_poem.append(word1)
 
         while True:
-            word2 = self.sample_word(self.transitions[(word0, word1)])
+            word2 = self.sample_word(self.transition_probabilities[(word0, word1)])
             if word2 == "END":
                 break
             machine_poem.append(word2)
@@ -129,8 +137,7 @@ class poems_Generator():
         return " ".join(machine_poem)
 
 
-if __name__== "__main__":
-    name = poems_Generator()
+if __name__ == "__main__":
+    random.seed(714)
+    name = PoemsGenerator()
     print(name.generator())
-
-
